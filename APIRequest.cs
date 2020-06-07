5186dd8a6dac7042a8cfd3c3ec748422bbfd1a3f
tayinde <tbiayinde@gmail.com>
2020-06-06 23:30:56 -0400

    Send files

---
+++ APIRequest.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace APIRequest
+{
+    class DataToSend<type>
+    {
+        public type data;
+        public DataToSend(type data)
+        {
+            this.data = data;
+        }
+    }
+    public static class Request
+    {
+        static public dynamic city;
+        static public async Task Send(string city)
+        {
+            string apiKey = "4a4a79f8b83a4125b46222602200606";
+            string url = $"https://api.worldweatheronline.com/premium/v1/tz.ashx?q={city}&key={apiKey}&format=json";
+            DataToSend<string> info = new DataToSend<string>("No data needed currently.");
+            string json = JsonConvert.SerializeObject(info);
+            StringContent data = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpClient Client = new HttpClient();
+            var response = await Client.PostAsync(url, data);
+            var timeData = response.Content.ReadAsStringAsync().Result;
+            JObject time = JObject.Parse(timeData);
+            Request.city = time;
+        }
+    }
+}
---
+++ MainWindow.cs
@@ -0,0 +1,52 @@
+using System;
+using APIRequest;
+using Gtk;
+using UI = Gtk.Builder.ObjectAttribute;
+using Application = Gtk.Application;
+namespace gtkAppTest
+{
+    class MainWindow : Window
+    {
+        [UI] private Label _label1 = new Label () {
+            UseMarkup = true
+        };
+        [UI] private Button _button1 = null;
+        [UI] private Entry city = null;
+
+        public MainWindow() : this(new Builder("MainWindow.glade")) { }
+        private MainWindow(Builder builder) : base(builder.GetObject("MainWindow").Handle)
+        {
+            builder.Autoconnect(this);
+            CssProvider css = new CssProvider();
+            Gtk.StyleContext.AddProviderForScreen(Gdk.Screen.Default, css, Gtk.StyleProviderPriority.Application);
+            css.LoadFromPath ("./css/gtk.css");
+            DeleteEvent += Window_DeleteEvent;
+            _button1.Clicked += RenderCityData;
+            city.Activated += RenderCityData;
+        }
+
+        private void Window_DeleteEvent(object sender, DeleteEventArgs a)
+        {
+            Console.WriteLine($"{Application.ApplicationId}: App closed");
+            Application.Quit();
+        }
+
+        private async void RenderCityData(object sender, EventArgs a)
+        {
+            var place = city.Text.ToLower();
+            city.Text = "";
+            await Request.Send(place);
+            if (Request.city["data"]["error"] != null)
+            {
+                _label1.Text = "Location was not found.";
+            } else
+            {
+                string location = Request.city["data"]["request"][0]["query"];
+                string time = Request.city["data"]["time_zone"][0]["localtime"];
+                string offset = Request.city["data"]["time_zone"][0]["utcOffset"];
+                _label1.Text = $"{char.ToUpper(location[0])}{location.Substring(1, location.Length - 1)}\n\nDate: {time}\nUTC Offset: {offset}";
+            }
+            Console.WriteLine($"{Application.ApplicationId}: Request attempt executed.");
+        }
+    }
+}
