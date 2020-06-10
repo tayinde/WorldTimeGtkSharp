c6d49a696c38c31bc455d12ebf4766df695d9e40
tayinde <tbiayinde@gmail.com>
2020-06-09 20:15:32 -0400

    choice between testing api key and my personal key

--- ChooseKey.cs
+++ ChooseKey.cs
@@ -5,7 +5,7 @@ namespace APIRequest
 		public string value;
 		public void setValue(string req)
 		{
-			string result = (req == "public key") ? "4a4a79f8b83a4125b46222602200606" : req;
+			string result = (req == "public key") ? "35458aadabe8465c98501432201006" : req;
 			this.value = result;
 		}
 	}
