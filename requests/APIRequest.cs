e46249d01743d38d25f6259c430003e91ed91503
tayinde <tbiayinde@gmail.com>
2020-08-09 10:59:10 -0400

    removed secret

--- requests/APIRequest.cs
+++ requests/APIRequest.cs
@@ -13,7 +13,7 @@ namespace APIRequest
         static public async Task Send(string city)
         {
             Key key = new Key();
-            key.setValue("public key"); // "public key" can be replaced with a custom key
+            key.setValue("your key"); // "public key" can be replaced with a custom key
             string apiKey = key.value;
             string url = $"https://api.worldweatheronline.com/premium/v1/tz.ashx?q={city}&key={apiKey}&format=json";
             DataToSend<string> info = new DataToSend<string>("No data needed currently.");
